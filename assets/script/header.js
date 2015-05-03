2708200e4b781341718b84fff46bbeca324215f8
Frederique Clement <clementfj@gmail.com>
2015-05-03 16:52:27 +0200

    réécriture globale du JS

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,5 +1,5 @@
 $(function(){
-	
+
 	// Déclaration de differentes dimensions
 	var heightWindow = window.innerHeight;
 	var widthWindow = window.innerWidth;
@@ -12,19 +12,6 @@ $(function(){
 	var headerGutter;
 	window.mainNavFlag = false;
 	$(document).ready(function(){
-		/*  Navigation click animate  */
-		//$headerGutter = $('nav').height()-1;
-		$('.list-inline').each(function(){
-			var $this = $(this);
-			$this.find('a').each(function(){					
-				var $parent = $(this);
-				while(!$parent.hasClass('list-inline')){
-					if($parent.get(0).tagName == 'ul'){							
-					}
-					$parent=$parent.parent();
-				}					
-			});				
-		});
 		chronos_sticky();
 	});
 
@@ -36,65 +23,51 @@ $(function(){
 			window.mainNavFlag = true;
 			var $mainWrapper = $('.main-body-wrap');
 			var $topScroll = $target.offset().top - $('nav').height()+1;
-			$('html, body').animate({'scrollTop' : $topScroll}, 800);	
+			$('html, body').animate({'scrollTop' : $topScroll}, 800);
 		}
 	});
 
-	$(window).load(function(){
-		$headerGutter = $('nav').height()-1;
-		var target = window.location.hash;
-		var $target = $(target);
-		if($('.main-body-wrap').length > 0){
-			if(target != '' && $target.length != 0) {
-				window.mainNavFlag = true;
-				var $mainWrapper = $('.main-body-wrap');
-				var $topScroll = $target.offset().top - $('nav').height()+1;
-				$('html, body').animate({'scrollTop' : $topScroll}, 800);	
-			}
+	$headerGutter = $('nav').height()-1;
+	var target = window.location.hash;
+	var $target = $(target);
+	if($('.main-body-wrap').length > 0){
+		if(target != '' && $target.length != 0) {
+			window.mainNavFlag = true;
+			var $mainWrapper = $('.main-body-wrap');
+			var $topScroll = $target.offset().top - $('nav').height()+1;
+			$('html, body').animate({'scrollTop' : $topScroll}, 800);
 		}
-		chronos_sticky();
-	});
+	}
+	chronos_sticky();
 
 	$(window).resize(function(){
 		chronos_sticky();
-	});
-
-	$(window).scroll(function(){
+	}).scroll(function(){
 		chronos_sticky();
 		window.mainNavFlag = true;
 	});
 
 	function chronos_sticky(){
-		var wind_scr = $(window).scrollTop();
+		var $wind_scr = $(window).scrollTop();
 		var endIndex = $('#index').height();
-		/*if($(window).width() > 1140){*/
-			if(wind_scr > endIndex) $('nav').removeClass('sticky');
+			if($wind_scr > endIndex) $('nav').removeClass('sticky');
 			else $('nav').addClass('sticky');
-
-		/*}
-		else{
-			$('nav').removeClass('sticky');
-		}*/
-		var $windScr = $(window).scrollTop();
+		
 		var $windowHeight = $(window).height();
 		if($('.main-body-wrap').length > 0){
-			$('.list-inline a').each(function(){			
+			$('.list-inline a').each(function(){
 				if (window.mainNavFlag == true) {
 					if($(this).attr('href').indexOf('#') >= 0){
 						var $offsetElement = $($(this).attr('href').substr($(this).attr('href').indexOf('#')));
 						var $offsetTop = $offsetElement.offset().top;
-						if(($offsetTop >= $windScr && $offsetTop <= $windScr + 1/8 * $windowHeight) || 
-							($offsetTop+$offsetElement.height() >= $windScr + 1/8 * $windowHeight && $offsetTop+$offsetElement.height() <= $windScr + $windowHeight)){
+						if(($offsetTop >= $wind_scr && $offsetTop <= $wind_scr + 1/8 * $windowHeight) || ($offsetTop+$offsetElement.height() >= $wind_scr + 1/8 * $windowHeight && $offsetTop+$offsetElement.height() <= $wind_scr + $windowHeight)){
 							$('.list-inline li').removeClass('active');
 							$(this).parent().addClass('active');
 						}
-						
 					}
 				}
 			});
 		}
 	}
-	$(function() {
-		$('.cell').popin();
-	});
+	$('.cell').popin();
 });
