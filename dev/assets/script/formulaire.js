30e493ada2a6642c55c5309a52c7505cb08045ca
Frederique Clement <clementfj@gmail.com>
2015-06-03 15:49:18 +0200

    Modification du script pour afficher les tooltip en haut

--- dev/assets/script/formulaire.js
+++ dev/assets/script/formulaire.js
@@ -3,55 +3,55 @@ $(function() {
 
    function checkName() {
       if($.trim($('form').find('#name').val()).length >= 3) {
-         if(!$('form').find('#name').next('.tooltip').hasClass('hidden'))
-            $('form').find('#name').next('.tooltip').addClass('hidden');
+         if(!$('form').find('#name').prev('.tooltip').hasClass('hidden'))
+            $('form').find('#name').prev('.tooltip').addClass('hidden');
       }
       else {
          var message = "Chaîne trop courte. Il faut un minimum de 3 caractères.";
          if($.trim($('form').find('#name').val()).length < 1) {
             message = "Ce champ est requis.";
          }
-         $('form').find('#name').next('.tooltip').text(message).removeClass('hidden');
+         $('form').find('#name').prev('.tooltip').text(message).removeClass('hidden');
       }
    }
    // @TODO Faire la vérification de l-email
    function checkMail() {
       if($.trim($('form').find('#email').val()).length >= 8) {
-         if(!$('form').find('#email').next('.tooltip').hasClass('hidden'))
-            $('form').find('#email').next('.tooltip').addClass('hidden');
+         if(!$('form').find('#email').prev('.tooltip').hasClass('hidden'))
+            $('form').find('#email').prev('.tooltip').addClass('hidden');
       }
       else {
          var message = "Chaîne trop courte. Il faut un minimum de 8 caractères.";
          if($.trim($('form').find('#email').val()).length < 1) {
             message = "Ce champ est requis.";
          }
-         $('form').find('#email').next('.tooltip').text(message).removeClass('hidden');
+         $('form').find('#email').prev('.tooltip').text(message).removeClass('hidden');
       }
    }
    function checkObject() {
       if($.trim($('form').find('#object').val()).length >= 5) {
-         if(!$('form').find('#object').next('.tooltip').hasClass('hidden'))
-            $('form').find('#object').next('.tooltip').addClass('hidden');
+         if(!$('form').find('#object').prev('.tooltip').hasClass('hidden'))
+            $('form').find('#object').prev('.tooltip').addClass('hidden');
       }
       else {
          var message = "Chaîne trop courte. Il faut un minimum de 5 caractères.";
          if($.trim($('form').find('#object').val()).length < 1) {
             message = "Ce champ est requis.";
          }
-         $('form').find('#object').next('.tooltip').text(message).removeClass('hidden');
+         $('form').find('#object').prev('.tooltip').text(message).removeClass('hidden');
       }
    }
    function checkMessage() {
       if($.trim($('form').find('#message').val()).length >= 10) {
-         if(!$('form').find('#message').next('.tooltip').hasClass('hidden'))
-            $('form').find('#message').next('.tooltip').addClass('hidden');
+         if(!$('form').find('#message').prev('.tooltip').hasClass('hidden'))
+            $('form').find('#message').prev('.tooltip').addClass('hidden');
       }
       else {
          var message = "Votre message est trop court. Mettez plus de détails";
          if($.trim($('form').find('#message').val()).length < 1) {
             message = "Ce champ est requis.";
          }
-         $('form').find('#message').next('.tooltip').text(message).removeClass('hidden');
+         $('form').find('#message').prev('.tooltip').text(message).removeClass('hidden');
       }
    }
 
