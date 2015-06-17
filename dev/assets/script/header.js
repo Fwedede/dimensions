c576d6e30dadabcc2338ff0c496cf57e7ea3a059
Steeve Clement <cl.steeve@gmail.com>
2015-06-17 17:23:21 +0200

    réécriture de la fonction pour le scroll clavier/souris. Et mise en place de commentaires

--- dev/assets/script/header.js
+++ dev/assets/script/header.js
@@ -1,16 +1,20 @@
+// Permets d'affecter une classe au menu pour savoir ou l'on se trouve
 function animationScroll() {
-	var windowOffset = $(window).scrollTop();
-	var endIndex = $('nav').outerHeight();
+	var windowOffset = $(window).scrollTop(); // Positionnement de la page à l'écran
+	var endIndex = $('nav').outerHeight(); // Taille du menu
 
-	if(windowOffset > endIndex)
-	$('nav').fadeIn(300);
-	else
-	$('nav').fadeOut(500);
+	if(windowOffset > endIndex) {
+		$('nav').fadeIn(300);
+	}
+	else {
+		$('nav').fadeOut(500);
+	}
 
 	$('section').each(function(i, lmt) {
+		// Si le haut de la section n'est pas visible (par ce que trop haut dans l'écran)
 		if($(lmt).offset().top - endIndex <= windowOffset) {
+			// Si on est dans la #home
 			if(i === 0) {
-				// Si on est dans la #home
 				$('a[href^="#"]').parent().removeClass('active');
 			}
 			else {
@@ -20,50 +24,80 @@ function animationScroll() {
 	});
 }
 
+// Réajuste la taille minimale des sections en fonction de la taille de l'écran
 function resizeSection() {
-	var heightWindow = window.innerHeight;
-	var heightHome = $('header section').outerHeight();
+	var heightWindow = window.innerHeight; // Taille de la fenetre
+	var heightHome = $('header section').outerHeight(); // Taille de la section du header
 
-	$('header').outerHeight(heightWindow);
 	$('main section').css('min-height', heightWindow - $('nav').outerHeight());
-	$('header section').css('padding-top', ((heightWindow - heightHome) / 2.5));
+	// @TODO pourquoi la valeur 2.5 divise le tout ?
+	$('header section').outerHeight(heightWindow).css('padding-top', ((heightWindow - heightHome) / 2.5));
 }
-function up() {
-	if(!$('.popin-bg').is(':visible')){
-		var anchors = $('section');
-		if($(window).scrollTop() - $('nav').outerHeight() > 0) {
 
-			for (var i = anchors.length - 1; i >= 0; i--) {
-				if($(anchors[i]).offset().top < $(window).scrollTop()) {
-					$('body').animate({'scrollTop' : Math.round($(anchors[i]).offset().top) - $('nav').outerHeight()}, 800);
+// Animation spéciale pour le scroll de la page (clavier et souris)
+function scrollEffect(event) {
+	// Si scroll la souris, ou qu'on appuie sur la barre espace ou une des fleches directionnelles
+	if(!$('textarea, input').is(':focus') && ((event.keyCode == 32 || event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40)) || (event.originalEvent.wheelDelta > 0 || event.originalEvent.wheelDelta <= 0)) {
+		event.preventDefault();
 
-					i = -1;
+		$('section').each(function(i, lmt) {
+			// Si le haut de la section est plus haut de ce qu'on voit ET que le bas de la section est plus bas de ce qu'on voit
+			if(($(lmt).offset().top - $('nav').outerHeight() <= $(window).scrollTop()) && $(lmt).offset().top + $(lmt).outerHeight() - $('nav').outerHeight() > $(window).scrollTop()) {
+
+				// Si la section n'est pas affiché entièrement
+				if($(lmt).outerHeight() > $(window).innerHeight()) {
+
+					// Si on n'est pas tout en haut ET qu'on scroll vers le haut ou qu'on appuie sur les flèches 'gauche' ou 'haut'
+					if(i !== 0 && ((event.keyCode == 37 || event.keyCode == 38)) || event.originalEvent.wheelDelta > 0) {
+						// Si on ne voit pas encore le bas de la section
+						if($(lmt).offset().top - $('nav').outerHeight() < $(window).scrollTop()) {
+							$('body').animate({'scrollTop' : $(lmt).offset().top - $('nav').outerHeight()}, 800);
+						}
+						// On est en bas de la section
+						else {
+							$('body').animate({'scrollTop' : Math.round($('section').eq(i-1).offset().top) - $('nav').outerHeight()}, 800);
+						}
+					}
+					// Si on n'est pas tout en bas ET qu'on scroll vers le bas ou qu'on appuie sur la touche espce ou une des flèches 'droite' ou 'bas'
+					else if(($('section').length - 1 != i) && ((event.keyCode == 32 || event.keyCode == 39 || event.keyCode == 40)) || event.originalEvent.wheelDelta <= 0) {
+						// Si on ne voit pas encore le bas de la section
+						if(($(lmt).offset().top + $(lmt).outerHeight() - $('nav').outerHeight() - $(window).scrollTop()) > $(window).innerHeight()) {
+							$('body').animate({'scrollTop' : $(window).innerHeight() + $(window).scrollTop() - $('nav').outerHeight()}, 800);
+						}
+						// On est en bas de la section
+						else {
+							$('body').animate({'scrollTop' : Math.round($('section').eq(i+1).offset().top) - $('nav').outerHeight()}, 800);
+						}
+					}
 				}
+				// La section fait la taille qu'on voit (pas plus grande)
+				else {
+					// Si on n'est pas tout en haut ET qu'on scroll vers le haut ou qu'on appuie sur les flèches 'gauche' ou 'haut'
+					if(i !== 0 && ((event.keyCode == 37 || event.keyCode == 38)) || event.originalEvent.wheelDelta > 0) {
+						$('body').animate({'scrollTop' : Math.round($('section').eq(i-1).offset().top) - $('nav').outerHeight()}, 800);
+					}
+					// Si on n'est pas tout en bas ET qu'on scroll vers le bas ou qu'on appuie sur la touche espce ou une des flèches 'droite' ou 'bas'
+					else if(($('section').length - 1 != i) && ((event.keyCode == 32 || event.keyCode == 39 || event.keyCode == 40)) || event.originalEvent.wheelDelta <= 0) {
+						$('body').animate({'scrollTop' : Math.round($('section').eq(i+1).offset().top) - $('nav').outerHeight()}, 800);
+					}
+				}
+				return false;
 			}
-		}
-	}
-}
-function down() {
-	if(!$('.popin-bg').is(':visible')){
-		var anchors = $('section');
-		for (var j = 0; j < anchors.length; j++) {
-			if(Math.floor($(anchors[j]).offset().top - $('nav').outerHeight()) > $(window).scrollTop()) {
-				$('body').animate({'scrollTop' : Math.round($(anchors[j]).offset().top) - $('nav').outerHeight()}, 800);
+		});
 
-				j = anchors.length;
-			}
-		}
 	}
 }
+
 
 $(function() {
-	resizeHome();
+	resizeSection();
 
-	$('a[href^="#"]').click(function(e) {
-		e.preventDefault();
 
-		var target = $(e.currentTarget.hash).offset().top;
-		var menuHeight = $('nav').outerHeight() - 1;
+	$('a[href^="#"]').click(function(event) {
+		event.preventDefault();
+
+		var target = $(event.currentTarget.hash).offset().top;
+		var menuHeight = $('nav').outerHeight();
 
 		$('body').animate({'scrollTop' : target-menuHeight}, 800, function() {
 			if($(window).innerWidth() <= 768) {
@@ -72,25 +106,16 @@ $(function() {
 		});
 	});
 
+
 	$('.menu_query img').mouseover(function(event) {
 		$(this).next('ul').removeAttr('style');
 	});
 
 
-	$('body').keydown(function(e) {
-		if(e.keyCode == 32 || e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40) {
-			if(!$('textarea, input').is(':focus')) {
-				e.preventDefault();
-
-				if(e.keyCode == 37 || e.keyCode == 38) {
-					up();
-				}
-				if(e.keyCode == 32 || e.keyCode == 39 || e.keyCode == 40) {
-					down();
-				}
-			}
-		}
+	$('body').keydown(function(event) {
+		scrollEffect(event);
 	});
+
 
 	$(window).resize(function(){
 		animationScroll();
@@ -101,12 +126,7 @@ $(function() {
 		}
 	}).scroll(function(){
 		animationScroll();
-	}).on('mousewheel', function(e) {
-		if(e.originalEvent.wheelDelta > 0) {
-			up();
-		}
-		if(e.originalEvent.wheelDelta <= 0) {
-			down();
-		}
+	}).on('mousewheel', function(event) {
+		scrollEffect(event);
 	});
 });
